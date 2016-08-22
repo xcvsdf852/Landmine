9a56531da3fbd6691bc34512635ec6c276b6626c
Eric <xcvsdf852@gmail.com>
2016-08-22 08:50:25 +0000

    1200炸彈

--- Landmine1200.php
+++ Landmine1200.php
@@ -1,18 +1,16 @@
 <?php
-
-for ($x = 0; $x <= 9; $x++) {
-    for ($y = 0; $y <= 9; $y++) {
+$Time1 = microtime(true);
+for ($x = 0; $x <= 49; $x++) {
+    for ($y = 0; $y <= 59; $y++) {
         $map[$x][$y] = 0;
     }
 }
-
 #跑亂數
-
 $Rand = Array();
-for ($i=1; $i<=40; $i++){
+for ($i=1; $i<=1200; $i++){
 
-    $randvalX=rand(0,9);
-    $randvalY=rand(0,9);
+    $randvalX=rand(0,49);
+    $randvalY=rand(0,59);
     $randval = $randvalX.','.$randvalY;
      if (in_array($randval, $Rand)) { //如果已產生過迴圈重跑
         $i--;
@@ -20,28 +18,35 @@ for ($i=1; $i<=40; $i++){
         $Rand[] = $randvalX.','.$randvalY; //若無重復則 將亂數塞入陣列
     }
 }
-
-// echo (int)'123';
-// exit;
-
-// var_dump($Rand);
 #將不重複的亂數放入
 foreach($Rand as $v){
     $temp = explode(",",$v);
     $map[$temp[0]][$temp[1]] = "M";
 }
-$count = 0;
-foreach($map as $v1){
-    foreach($v1 as $v2){
-        if($v2 == "M"){
-            var_dump($v2);
-            //  $count++;
+#計算炸彈數量
+foreach ($map as $key_1 => $val_1) {
+    foreach($val_1 as $key_2 => $val_2){
+        if($map[$key_1][$key_2] == '0'){
+            $map[$key_1][$key_2] = Score($map,$key_1,$key_2);
         }
     }
-
 }
-// echo $count;
-exit;
+
+
+$echostr = '';
+foreach ($map as  $val_1){
+    foreach($val_1 as $key_2 => $val_1){
+        $echostr .= $val_1;
+    }
+    $echostr .= 'N';
+}
+
+
+echo substr($echostr,0,-1);
+$Time2 = microtime(true);
+echo "<hr>";
+echo $Time2 - $Time1;
+echo "<hr>";
 
 echo '<table>';
 foreach($map as $v1){
@@ -55,29 +60,6 @@ echo '</table>';
 
 echo '<hr>';
 
-foreach ($map as $key_1 => $val_1) {
-    foreach($val_1 as $key_2 => $val_2){
-        if($map[$key_1][$key_2] == '0'){
-            $map[$key_1][$key_2] = Score($map,$key_1,$key_2);
-        }
-    }
-    // echo '<br>';
-}
-
-
-
-
-echo '<table>';
-foreach($map as $v1){
-    echo '<tr>';
-    foreach($v1 as $v2){
-        echo '<td>'.$v2.'<td>';
-    }
-    echo '<tr>';
-}
-echo '</table>';
-// var_dump($map);
-// exit;
 
 function Score($origin,$x,$y){
     $temp = 0;
@@ -109,4 +91,3 @@ function Score($origin,$x,$y){
 
     return  $temp;
 }
-
